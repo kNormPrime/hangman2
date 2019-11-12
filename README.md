1a9b6a3e1ac1bd0d5e145401294e540b4ae0f360
kNormPrime <knormile@albany.edu>
2019-11-11 23:39:32 -0500

    HANGMAN Project in working order

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hangman2
+
+## description
+This project is a simple console interface version of the game hangman, in which the user attempts to guess an unknown word one letter at a time by submitting input of letter guesses when prompted to do so.  For each unknown word, designated as a round in the game, the user loses the round after 6 incorrect letter guesses, and wins if all letters are guessed correctly before reaching the incorrect guess count limit.
+
+
+### how to interact with the console
+To begin the game, the user types "node ." into the console and hits the "enter" button.  When prompted, the user types in an alphabetic keyboard character as a letter guess and hits the "enter" button.  To stop playing, the user hits "control+c" buttons.
--- index.js
+++ index.js
@@ -1,48 +1,22 @@
-//const readlineSync = require('readline-sync');
+
+//////////////////// HANGMAN GAME //////////////////////////////////////////////////////////////
+/////////////////////////program////////////////////////// by kNormPrime, Nov 2019 //////////////
 
 //////////////////  bring in outside required tools ////////////////
 const prompt = require('readline-sync');
 const wordbank = require("./word-bank.json");
 ////////////////////////////////////////////////////////////////////
-/*
-///////////////////////////////////// After 'welcome to hangman' //////////////////////////////////////
-let secretWORD =  wordbank[ Math.floor(Math.random()*wordbank.length) ] ;
 
-*/
-/////////////////////////////////////////////////////////////////////////////////////////////
-function  visual_displays (secretWORD) 
-{
-strBorder = "";   brdAry = [];  blkAry = [];  strBLANK = "" ;  hidaWrd = ""; hidAry =[] ;
-for(let i = 0; i<secretWORD.length ; i++)  {brdAry[i]=`?`; blkAry[i]=" "; hidAry[i]=`*`;}
-strBorder=brdAry.join(``);   strBLANK = blkAry.join(``);  hidaWrd=hidAry.join(``);
-
-const secretBOX = (whatsTHIS) =>
- {
-console.clear();
-console.log(`|               ???`+ strBorder +`???`);
-console.log(`|               ??` + strBLANK +  `  ??`);
-console.log(`|               ?? `+ whatsTHIS +` ??`);
-console.log(`|               ??` + strBLANK +  `  ??`);
-console.log(`|               ???`+ strBorder +`???`);
-console.log(`|`) ;
- } ;
-
-const box_1 = () =>  {
-secretBOX(hidaWrd) ;
-console.log(`|  Who might you be, a mystery word perhaps? `);
-console.clear();      };
-
-const box_2 = () =>  {
-secretBOX(secretWORD) ;
-console.log(`|  The mystery word is revealed! End of this round of HANGMAN.   `);
-console.clear();      };
-
-}
-////////////////////end visual_display //////////////////////////////////////////////////////////
-
-// call start function    start(myQuest) ;
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
+////////////////////// global variables //////////////////////
+let guessWRONGcount = 0 ;   let guessRIGHTcount = 0 ; 
+let GUESSED_rt =[] ;  let all_GUESSES = [``] ;  //arrays of user guessed characters
+let testWORD ;  //randomly generated string gets passed to and assigned in round_Initialization function
+let testGuess ; //character inputted by user gets passes to and assigned in startTURN function
+let rounds_played = 0 ;  let rounds_won = 0 ;  // displays after round conclusion
+let newRound_or_Continue = 1 ;  // condition for continuing or new round
+//////////////////////////////////////////////////////////////
 
 function text_Outputs (id) 
 {   
@@ -52,26 +26,22 @@ function text_Outputs (id)
     const str2 = `\nPlease guess a letter: ` ;
     const str3 = `\nYou guessed CORRECT!\n` ;
     const str4 = `\nYou guessed WRONG...\n` ;
-    const str5 = `` ;
-    const situational_String = [str0,str1,str2,str3,str4,str5] ;
+    const str5 = `\nYou have won `+ rounds_won + ` round(s) out of ` + rounds_played + ` round(s) played. ` ;
+    const str6 = `\nThe mystery word was -->  ` ;
+    const daEND = `\n The END. This round is concluded.` ;
+    const situational_String = [str0,str1,str2,str3,str4,str5,str6, daEND] ;
     return situational_String [id] ;
 }
 /////////////////////////////
-function visual_diplays ()
-{
+// more output strings put into functions
+function guessAgain () { console.log (`\nround in progress...`);    }
 
-}
-////////////////////// global variables //////////////////////
-let guessWRONGcount = 0 ;   let guessRIGHTcount = 0 ; 
-let GUESSED_rt =[] ;  let all_GUESSES = [``] ;  //arrays of user guessed characters
-let testWORD ;  //randomly generated string gets passed to and assigned in round_Initialization function
-let testGuess ; //character inputted by user gets passes to and assigned in startTURN function
-//////////////////////////////////////////////////////////////
+const killplayer = (word)=> {rounds_played++ ; console.log("You are dead..."); 
+                         console.log(text_Outputs(6) + word + text_Outputs(7) + text_Outputs(5));  } ;
+const playerLives = (word)=>{rounds_played++ ; rounds_won++ ; 
+                        console.log(text_Outputs(6) + word + text_Outputs(7) + text_Outputs(5));   } ; 
 
-function guessAgain () { console.log("get input \n\n");    }
-
-const killplayer = ()=> {console.log("You are dead \n\n"); setTimeout(()=>{round_Initialization();},5000); } ;
-const playerLives = ()=>{ setTimeout(()=>{round_Initialization();},5000); } ;
+                      
 
 ////////////////// this function mediates between turns of guess input //////////////////////////////////////////
 let startTURN = (letter) => 
@@ -80,32 +50,64 @@ let startTURN = (letter) =>
 
       if(testGuess === "INVALID") { gettaGUESS() ; }  // returns user to input prompt
       else { GuessPlaysGame() ;}
-      //   
+      //  after above input validation occurs, GuessPlaysGame runs and alters
+      //  global variables guessRIGHTcount and guessWRONGcount,
+      //   which are then analyzed by guessesRemain below 
+      // it will return appropriate output to the user,
+      // as well as pass a parameter off  for use determining
+      //  continuance of round or new round 
        
    /////// checks wrong guess count //////
   const guessesRemain = ()=>   
-   {  const daEND = `\n The END. This round is concluded.` ;
+   {  let swtchParam ;
+    
       const jabb = `\n...you may have won this round, but you won't be so lucky next time...` ;
-      if(guessRIGHTcount=== testWORD.length)
-          { console.log(`Congratulations! You WIN!`+ daEND + jabb); playerLives() ; } 
-      else if(guessWRONGcount < 7)
-          {console.log(" guessAgain... ") ; guessAgain(); gettaGUESS(); }
-      else {console.log(" killplayer! "  + daEND ); killplayer() ;}
-   } ;
+      
+     if(guessRIGHTcount=== testWORD.length && guessWRONGcount < 6) {swtchParam = 1 ;}
+     else if (guessRIGHTcount !== testWORD.length && guessWRONGcount >= 6 ) {swtchParam = 2;}
+     else {swtchParam = 0 ;}
+
+
+        let round_control;
+      switch (swtchParam)
+      {case 1:
+        {round_control = 1 ; console.log(`\n Congratulations! You WIN!`+  jabb); playerLives(testWORD) ;}
+        break ;
+       case 2:
+         {round_control = 1 ; console.log("\n killplayer! "  ); killplayer(testWORD) ;}
+         break ;
+       default:
+          {round_control = 2 ; console.log("\n guessAgain... ") ; guessAgain(); }      
+       } //end switch
+       return round_control ;
+   } ;   // end guessesRemain
    //////////////////////////////////////
-       guessesRemain();
+       newRound_or_Continue = guessesRemain();  
+       
+       return continue_OR_new(newRound_or_Continue) ;
  };
 //////////////////  end startTURN function /////////////////////////////////////
+
+function continue_OR_new (what2do)
+{  let doThis ;
+    if(what2do === 1) { doThis = round_Initialization() ; }
+    else if(what2do === 2) { doThis = gettaGUESS() ; }
+
+    return setTimeout( ()=>{return  doThis ; } , 4000) ;
+}
+   continue_OR_new(newRound_or_Continue) ; // 1st time at game start
+    
+
 
 ///////////////////// function for getting user input //////////////////////////
 function gettaGUESS ()  
 {   let send_it ;
     setTimeout( ()=>
-    {
+    {console.log(`mystery word -->   ` + GUESSED_rt.join(``)) ;
     const myReQuest = prompt.question(`|  USER, enter a letter guess:   `);
     console.clear();  send_it = myReQuest ; 
     startTURN(send_it);   // take input to function that mitigates each turn or guess
-    } , 5000) ;
+    } , 1000) ;
     
 } 
 ///////////////////// end gettaGUESS //////////////////////////////////////
@@ -150,124 +152,76 @@ function inputValidator (userEntry)
 ////////////////////////////////////////////////////////////////////////////////////////
 function round_Initialization () 
 {
-    console.log( text_Outputs(0) + text_Outputs(1));  
-    console.log(`The game is in play when you see: ("| USER, enter a letter guess: ")`) ; 
-    setTimeout(()=>{console.log(`_`)},1000); setTimeout(()=>{console.log(`  _`)},2000); 
-    setTimeout(()=>{console.log(`    _`)},3000); setTimeout(()=>{console.log(`      _`)},4000);
-    setTimeout(()=>{console.clear();}, 6000);
-    //////////////////////////////// paste here ////////////////////
-///////////////////////////////////// After 'welcome to hangman' //////////////////////////////////////
+    // pick a new word from list 
 let secretWORD =  wordbank[ Math.floor(Math.random()*wordbank.length) ] ;
-//visual_displays(secretWORD).box_1()  ;
-/*
-strBorder = "";   brdAry = [];  blkAry = [];  strBLANK = "" ;  hidaWrd = ""; hidAry =[] ;
-for(let i = 0; i<secretWORD.length ; i++)  {brdAry[i]=`?`; blkAry[i]=" "; hidAry[i]=`*`;}
-strBorder=brdAry.join(``);   strBLANK = blkAry.join(``);  hidaWrd=hidAry.join(``);
 
-const secretBOX= (whatsTHIS) =>
-{
-console.clear();
-console.log(`|               ???`+ strBorder +`???`);
-console.log(`|               ??` + strBLANK +  `  ??`);
-console.log(`|               ?? `+ whatsTHIS +` ??`);
-console.log(`|               ??` + strBLANK +  `  ??`);
-console.log(`|               ???`+ strBorder +`???`);
-console.log(`|`) ;
-} ;
-const tease = (`|  Who might you be, a mystery word perhaps?  `) ;
-setTimeout( ()=>{ secretBOX(hidaWrd); console.log(tease); } , 4000) ;
-*/
-
-/*
-let send_it ;
-setTimeout( ()=>
-{
-const myReQuest = prompt.question(`|  USER, enter a letter guess:   `);
-console.clear();  send_it = myReQuest ; 
-} , 7000) ;
- */
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
-    ///////////////////////////////// cut here ////////////////////
-    
-  // pick a new word from list  
-  randWORD = secretWORD;
+   // set the random word to a global variable
+  testWORD = secretWORD.toLowerCase(); // noticed a word in wordbank with capitalized letter
   // start guesses at 0                                 // reset visual displays
      guessWRONGcount = 0 ;     guessRIGHTcount = 0 ;    // visual_H_man = 0 ;
-                                all_GUESSES = [``] ;  // resets each round    
-   testWORD = randWORD;  // assign the value of the random word to a global variable
+      GUESSED_rt = [``] ;     // must reset here, because if a prior round's word is
+                                  // larger than the next round's, letters won't be cleared by the loop below  
+      all_GUESSES = [``] ;  // resets each round    
+   
     
-  for(let i=0; i<randWORD.length; i++) // make an array of unguessed characters
+  for(let i=0; i<testWORD.length; i++) // make an array of unguessed characters
      { GUESSED_rt[i] = ` _ ` ; }
-    
-     setTimeout( ()=> { 
-        console.log(`mystery word -->   ` + GUESSED_rt.join(``)) ;
-         gettaGUESS() ; } , 7000);  // promps user and collects input
 
-  //let temp = GUESSED_rt.forEach((val,idx,ary)=>{ ary[idx] = val + "_";})
+     console.log( text_Outputs(0) + text_Outputs(1));  
+     console.log(`The game is in play when you see: ("| USER, enter a letter guess: ")`) ; 
+    ////////// After 'welcome to hangman' /////////   
+     console.log(`\nnew round started...`);
+         gettaGUESS() ;  // promps user and collects input
+
 }           
 ///////////////////// end round initialization function ///////////////////////////////////////
 
-round_Initialization();
+
 
 
 ////////////////////////this function tests each letter guess against mystery word ///////////
 function GuessPlaysGame ()
-{  let daMain_output = "wrong!";  // to be used as a boolean test value
+{  let condition_of_input = "wrong!";  // to be used as a boolean test value
    all_GUESSES.push(testGuess) ; //records guess to validate against repeat guesses
-   // redundancy text_Outputs(2);
-    //let grabGuess = (letter) => { let guess = letter; return guess;} ;
-   // let testGuess =  grabGuess(`n`);
+  
    let daWord = testWORD.split('');   // put secret string into an array of characters
    for(let i=0; i<daWord.length; i++)
    {
    if(testGuess === daWord[i])
-       {text_Outputs(3); GUESSED_rt[i]=(daWord[i]); daMain_output = 'correct!' ; guessRIGHTcount++; }
+       {text_Outputs(3); GUESSED_rt[i]=(daWord[i]); condition_of_input = 'correct!' ; guessRIGHTcount++; }
    else{text_Outputs(4);  }
    } // end for loop
-            // we pass daMain_output as a boolean test value
+            // we pass condition_of_input as a boolean test value
    /* notice that the guessRIGHTcount is incremented inside the loop, as there may be more
        than one instance of the guessed letter occurring as we loop through the word
        below, the guessWRONGcount is incremented outside the loop, only showing
        that the guess failed one time due to not occuring once in the loop
     */
    let answerUSER = "" ;
-   if(daMain_output !== `correct!`) 
+   if(condition_of_input !== `correct!`) 
        { answerUSER = (text_Outputs(4)); guessWRONGcount++ ; }
    else{ answerUSER = (text_Outputs(3)); }  
 
-   let standings = {'current round standings' :  {'correct': guessRIGHTcount, 'incorrect': guessWRONGcount}   } ;
+   let standings = {'current round standings' :  {'correct' : guessRIGHTcount,  'incorrect' : guessWRONGcount}   } ;
    let score = JSON.stringify(standings); 
-   let alsoThis = {'bad guesses left' :  ((7-guessWRONGcount)+ ' remaining out of 7')};     
+   let alsoThis = {'bad guesses left' :  ((6-guessWRONGcount)+ ' remaining out of 6')};     
    let helpfulInfo = JSON.stringify(alsoThis); 
-  // let shout = ('\ You guessed: '+ testGuess+`   `+ guessWRONGcount +" incorrect    "+ guessRIGHTcount +" correct    "+ GUESSED_rt.join(``) +'\n');
+  
 
   ////////// MOST IMPORTANT OUTPUT LINEs OF CODE IN WHOLE PROGRAM  /////////////////////////////////////////////////
    let shout = ('\n You guessed: '+ testGuess+`          mystery word---> `+ GUESSED_rt.join(``)  + `\n` ) ;
-   console.log(answerUSER + shout +`\t\t\t\t` +score +`\t\t\t\t\t\t `+ helpfulInfo);
+   console.log(answerUSER + shout +`\t\t\t\t` +score +`\t\t\t\t\t\t\t\t\t `+ helpfulInfo);
    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-   console.log('hint: secret word is --> ' + testWORD);  // DELETE THIS !!!!
+
+   // If you would like to view the secret word while playing, un-comment line below...
+   // console.log('hint: secret word is --> ' + testWORD);  // comment out !!!!
    
 }
 ////////////////// end GuessPlaysGame function ///////////////////////////////////////////////////////////////////
 
-/*
-middle of program
-*/
- 
 
-/*
+//////////////////////////////  END PROGRAM ////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////// display at end of program  /////////////////////////////
-let briefDelayIn_ms = 5000 ;
-setTimeout( ()=>{ 
-console.clear();      
-console.log(`|               ???`+ strBorder +`???`);
-console.log(`|               ??` + strBLANK +  `  ??`);
-console.log(`|               ?? ` +secretWORD+` ??`);
-console.log(`|               ??` + strBLANK +  `  ??`);
-console.log(`|               ???`+ strBorder +`???`);               
-} , briefDelayIn_ms) ;
-////////////////////////////////////////////////////////////////////////////////////
 
-*/
